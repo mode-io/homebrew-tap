845ae2c74297f016b6dc337584fc20350b87cfe7
Sirui Zhang <siruizhangdev@gmail.com>
2026-04-15 11:17:26 +0800

    Refresh skill-manager v0.1.0 asset checksums

--- Formula/skill-manager.rb
+++ Formula/skill-manager.rb
@@ -7,12 +7,12 @@ class SkillManager < Formula
 
   on_arm do
     url "https://github.com/mode-io/skill-manager/releases/download/v0.1.0/skill-manager-v0.1.0-darwin-arm64.tar.gz"
-    sha256 "ade45fc3616a554cf7f8bd7dfe61f92387613a4dd633e810b48649afb1944130"
+    sha256 "e4f8eb666eb2440ec14544765149076ad83fec4e5b2d51e137509a749b2ee4b6"
   end
 
   on_intel do
     url "https://github.com/mode-io/skill-manager/releases/download/v0.1.0/skill-manager-v0.1.0-darwin-x64.tar.gz"
-    sha256 "b947e2cbedd7d507454ff4a8a07c5e2776f47e79c6b1395bf78d264ea9a4534d"
+    sha256 "f5c3dae8bccba11c47b3939c4d1ff283db57ab0a670ac7701b5305b7bcd6f2be"
   end
 
   def install
